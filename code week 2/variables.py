2c1bdec4682fa91528757ca871fe6e5185599e4d
AarkayITsolutions <alihassanali119683@gmail.com>
2025-06-28 13:29:19 +0500

    Adding week 2 code files

---
+++ code week 2/variables.py
@@ -0,0 +1,21 @@
+# Declaring personal profile data using variables
+
+first_name = "Hassan"
+last_name = "Ali"
+age = 22
+department = "Software Engineering"
+university = "PAF-IAST"
+cgpa = 3.8
+is_graduating = False
+
+# Full name using string concatenation
+full_name = first_name + " " + last_name
+
+# Display all details
+print("----- STUDENT PROFILE -----")
+print("Full Name:", full_name)
+print("Age:", age)
+print("Department:", department)
+print("University:", university)
+print("Current CGPA:", cgpa)
+print("Graduating This Year?", is_graduating)
