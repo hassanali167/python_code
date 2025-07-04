cf4bffa0810c708d52ade58669134845c382c5f9
AarkayITsolutions <alihassanali119683@gmail.com>
2025-07-04 10:55:43 +0500

    Adding week 3 code files

---
+++ codes week 3/operators.py
@@ -0,0 +1,10 @@
+a = 12
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Power:", a ** b)
+print("Floor Division:", a // b)
